fce5b69e50a3619dffd98af1e3351d7396564e29
multi05369 <tentingsanook@gmail.com>
2025-07-20 00:12:40 +0700

    feat: Finish Code Paramiko WIN/LIN

--- paramiko/code-LIN/paramikolab.py
+++ paramiko/code-LIN/paramikolab.py
@@ -1,19 +1,17 @@
-"""Example how to use Pramiko"""
+"""Example how to use Pramiko Version Linux"""
 
 import time
 import paramiko
 
-username = "admin" # DEVASC if use VM
-password = "your_password_here"  # Replace with your actual password
+username = "username"
+password = "supersecret"
 
-# Work better in Linux
-
-device_ip = ["ip1", "ip2", "ip3"]  # Replace with actual device IPs
+device_ip = ["your ip"]
 
 for ip in device_ip:
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip)
+    client.connect(hostname=ip, username=username, look_for_keys=True)
     print(f"Connected to {ip}")
     with client.invoke_shell() as ssh:
         print("Conntected to {}".format(ip))
